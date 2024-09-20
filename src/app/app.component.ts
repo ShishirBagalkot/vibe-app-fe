4a65819cf2296588971ae74189c91b1fe4f7bd3e
Shishir Bagalkot <59408046+Shishirbagalkot@users.noreply.github.com>
2024-09-20 13:42:06 +0530

    init fa icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'vibe-app';
+
+  private faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+      this.initFontAwesome();
+  }
+
+  private initFontAwesome(): void {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
